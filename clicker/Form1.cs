76ed062babbc571dbc262d958dc15705a9b8d7f7
przybylski.pawel <przybylski.pawel@STACJA39A-14>
2022-12-12 08:42:48 +0100

    Dodaj pliki projektów.

---
+++ clicker/Form1.cs
@@ -0,0 +1,27 @@
+﻿namespace clicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int licznik = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            licznik++;
+            label2.Text =  licznik.ToString();
+
+            if (licznik == 100)
+            {
+                label3.Text = "🤡";
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
